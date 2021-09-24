a0407a89b9339f6c6da3497d10148d74176f863c
crocokyle <me@kyle-dixon.com>
2021-09-24 01:32:29 -1000

    Update RVN wallet address and add pool stats chrome tab

--- NBMiner_39.5_Win/ravenpool.ninja.bat
+++ NBMiner_39.5_Win/ravenpool.ninja.bat
@@ -1,2 +1,2 @@
 start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile2
-nbminer -d 1 -a kawpow -o stratum+tcp://stratum.ravenpool.ninja:3333 -u RLvUDmnJu1eCYSadW5Tb8qRoTBt6dApCEP.3060
+nbminer -d 1 -a kawpow -o stratum+tcp://stratum.ravenpool.ninja:3333 -u RRkrBSYKGCok1ZHYMt7CXADEw3BvPFQ6Uf.3060
--- t-rex-0.21.6-win/ETH-flexpool.bat
+++ t-rex-0.21.6-win/ETH-flexpool.bat
@@ -1,3 +1,3 @@
-start chrome "https://www.flexpool.io/miner/eth/0x2eceacd95221e2b4e97acac24d725f8a529da391"
+start chrome "https://www.flexpool.io/miner/eth/0x2eceacd95221e2b4e97acac24d725f8a529da391" "https://ravenpool.ninja/dashboard.html#ravencoin/stats?address=RRkrBSYKGCok1ZHYMt7CXADEw3BvPFQ6Uf"
 "C:\Users\me\code\windows-mining-config\t-rex-0.21.6-win\t-rex.exe" --devices 0 --pl 75 --fan t:63 --cclock -502 --mclock 1300 --api-bind-http 0.0.0.0:1234 -a ethash -o stratum+ssl://eth-us-west.flexpool.io:5555 -u 0x2eceacd95221e2b4e97acac24d725f8a529da391 -p x -w Kyle-PC
 pause
